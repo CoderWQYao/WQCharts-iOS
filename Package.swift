9b38d666ce7586839663c020e9628cda60be0fa2
巅峰寂寞 <eternalg4@qq.com>
2020-03-07 16:42:29 +0800

    update package

--- Package.swift
+++ Package.swift
@@ -2,22 +2,25 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import SwiftUI
 
 let package = Package(
+    
     name: "WQCharts",
+    platforms: [
+        .iOS(.v8),
+    ],
     products: [
         .library(
             name: "WQCharts",
             targets: ["WQCharts"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/CoderWQYao/WQCharts-iOS.git", from: "1.0.0"),
-    ],
     targets: [
         .target(
             name: "WQCharts",
-            dependencies: [],
             path: "WQCharts/Classes"
         )
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
+    
 )
